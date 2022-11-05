163e951368f118fe39b7d41192516b353b1dbd58
Muhammad Irtiza <91867702+Irtiza751@users.noreply.github.com>
2022-11-05 14:23:34 +0500

    Create arrays.js

---
+++ Arrays/arrays.js
@@ -0,0 +1,10 @@
+/**
+* @Arrays 
+* Create a custom Array data structure with functions
+* 1. addItem() // add
+* 2. deleteItem() // delete
+* 3. getItem() // get
+* 4. updateItem() // update
+*/
+
+class MyArray {}

94d9ec03c03319105dd0180bed2b5d1250addf3b
Muhammad Irtiza <91867702+Irtiza751@users.noreply.github.com>
2022-11-07 10:08:32 +0500

    Add abstract method definitions of arrays.

--- Arrays/arrays.js
+++ Arrays/arrays.js
@@ -7,4 +7,22 @@
 * 4. updateItem() // update
 */
 
-class MyArray {}
+class MyArray {
+  constructor() {
+    const array = {};
+    const length = 0;
+  }
+  
+  // add item in to the array's last index
+  addItem() {}
+  
+  // delete the item from the array
+  // NOTE: if you pass the index it'll delete that particular item else remove the last item.
+  deleteItem() {}
+  
+  // get the item of provided index
+  getItem() {}
+  
+  // update item at a particular index.
+  updateItem() {}
+}
